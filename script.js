222a88df10a7db0aacf15347e15f243a56899279
Julia-08 <julia.d.paula@aluno.senai.br>
2025-06-17 17:14:07 -0300

    calculadora pronta

--- script.js
+++ script.js
@@ -1,2 +1,34 @@
+const peso = document.getElementById("peso");
+const altura = document.getElementById("altura");
+const resultado = document.getElementById("resultado");
+const imcform = document.getElementById("imcform");
 
+imcform.addEventListener("submit", function (event) {
+  event.preventDefault();
+  calcularIMC(peso.value, altura.value);
+});
 
+function calcularIMC(p, a) {
+  const conta = p / (a * a);
+  let classificacao = "";
+
+  if (conta <= 18.5) {
+    classificacao = "Abaixo do peso";
+  }
+   else if (conta <= 24.9) {
+    classificacao = "Peso normal";
+  }
+   else if (conta <= 29.9) {
+    classificacao = "Sobrepeso";
+  }
+   else if (conta <= 34.9) {
+    classificacao = "Obesidade I";
+  }
+   else if (conta <= 39.9) {
+    classificacao = "Obesidade II";
+  }
+   else {
+    classificacao = "Obesidade III";
+  }
+resultado.textContent = `Seu peso é ${p}, sua altura é ${a} e seu IMC é ${conta.toFixed(2)}, seu nível é ${classificacao}`
+}
